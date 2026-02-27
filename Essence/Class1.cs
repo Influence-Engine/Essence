4545674675fd9ac088ebc4e2a090a254da32a033
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 22:24:32 +0100

    yes i pushed my notes... :)

--- Essence/Class1.cs
+++ Essence/Class1.cs
@@ -2,6 +2,13 @@
 {
     public class Class1
     {
+        // Not a "game engine"
+        // But a good start to any project
 
+        // Basic structured classes (Vector2Int, Color, Rect, Quaternion, etc.)
+        // Simple wrapped classes (Window, Input, Camera)
+
+        // Good enough for most 2D projects that do not need an ECS
+        // Usecase: Aether, Resonance and more o.o
     }
 }
