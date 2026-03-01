74ee4ec7375d909e3db1818adf6c26766e95a4d5
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-03-01 09:07:59 +0100

    Its about time...

---
+++ Essence.Tests/TimeTests.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Essence.Tests
+{
+    [TestFixture]
+    public class TimeTests
+    {
+        [Test]
+        public void InitializeState()
+        {
+            Time.Reset();
+
+            Assert.That(Time.time, Is.EqualTo(0f));
+            Assert.That(Time.timeAsDouble, Is.EqualTo(0f));
+
+            Assert.That(Time.unscaledTime, Is.EqualTo(0f));
+
+            Assert.That(Time.deltaTime, Is.EqualTo(0f));
+            Assert.That(Time.deltaTimeAsDouble, Is.EqualTo(0f));
+
+            Assert.That(Time.frameCount, Is.EqualTo(0f));
+            Assert.That(Time.fixedFrameCount, Is.EqualTo(0f));
+
+            Assert.That(Time.timeScale, Is.EqualTo(1f));
+        }
+
+        [Test]
+        public void SingleUpdate_Float()
+        {
+            Time.Reset();
+
+            float deltaTime = 1f / 60f;
+            Time.Update(deltaTime);
+
+            Assert.That(Time.deltaTime, Is.EqualTo(deltaTime));
+            Assert.That(Time.unscaledDeltaTime, Is.EqualTo(deltaTime));
+            Assert.That(Time.time, Is.EqualTo(deltaTime));
+        }
+
+        [Test]
+        public void SingleUpdate_Double()
+        {
+            Time.Reset();
+
+            double deltaTime = 1.0 / 60.0;
+            Time.Update(deltaTime);
+
+            Assert.That(Time.deltaTimeAsDouble, Is.EqualTo(deltaTime));
+            Assert.That(Time.unscaledDeltaTimeAsDouble, Is.EqualTo(deltaTime));
+            Assert.That(Time.timeAsDouble, Is.EqualTo(deltaTime));
+        }
+
+        [Test]
+        public void TimeScale_Zero()
+        {
+            Time.Reset();
+            Time.timeScale = 0f;
+
+            float deltaTime = 1f / 60f;
+            Time.Update(deltaTime);
+
+            Assert.That(Time.deltaTime, Is.EqualTo(0f));
+            Assert.That(Time.unscaledDeltaTime, Is.EqualTo(deltaTime));
+
+            Assert.That(Time.time, Is.EqualTo(0f));
+            Assert.That(Time.unscaledTime, Is.EqualTo(deltaTime));
+        }
+
+        [Test]
+        public void TimeScale_Two()
+        {
+            Time.Reset();
+            Time.timeScale = 2f;
+
+            float deltaTime = 1f / 60f;
+            Time.Update(deltaTime);
+
+            Assert.That(Time.deltaTime, Is.EqualTo(deltaTime * 2));
+            Assert.That(Time.unscaledDeltaTime, Is.EqualTo(deltaTime));
+
+            Assert.That(Time.time, Is.EqualTo(deltaTime * 2));
+            Assert.That(Time.unscaledTime, Is.EqualTo(deltaTime));
+        }
+
+        [Test]
+        public void TimeScale_Ignore_Unscaled()
+        {
+            Time.Reset();
+            Time.timeScale = 5;
+
+            float deltaTime = 1f / 60f;
+            Time.Update(deltaTime);
+            Time.Update(deltaTime);
+
+            Assert.That(Time.unscaledDeltaTime, Is.EqualTo(deltaTime));
+            Assert.That(Time.unscaledTime, Is.EqualTo(deltaTime * 2));
+        }
+
+
+        [Test]
+        public void FrameCount_Increment()
+        {
+            Time.Reset();
+
+            float deltaTime = 1f / 60f;
+            Time.Update(deltaTime);
+            Time.Update(deltaTime);
+            Time.Update(deltaTime);
+
+            Assert.That(Time.frameCount, Is.EqualTo(3));
+        }
+
+        #region Constructors
+
+        [Test]
+        public void Constructor_SetsValuesCorrectly()
+        {
+            var v = new Vector2(6f, 7f);
+
+            Assert.That(v.x, Is.EqualTo(6f));
+            Assert.That(v.y, Is.EqualTo(7f));
+        }
+
+        [Test]
+        public void Constructor_InitializesToZero()
+        {
+            var v = new Vector2();
+
+            Assert.That(v.x, Is.EqualTo(0f));
+            Assert.That(v.y, Is.EqualTo(0f));
+        }
+
+        #endregion
+
+        #region Indexer
+
+        [Test]
+        public void Indexer_Get_ReturnsCorrectComponent()
+        {
+            var v = new Vector2(6f, 7f);
+
+            Assert.That(v[0], Is.EqualTo(6f));
+            Assert.That(v[1], Is.EqualTo(7f));
+        }
+
+        [Test]
+        public void Indexer_Set_SetsCorrectComponent()
+        {
+            var v = new Vector2();
+
+            v[0] = 6f;
+            v[1] = 7f;
+
+            Assert.That(v.x, Is.EqualTo(6f));
+            Assert.That(v.y, Is.EqualTo(7f));
+        }
+
+        #endregion
+    }
+}
---
+++ Essence/Time.cs
@@ -0,0 +1,193 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Essence
+{
+    /// <summary>A static time management class. Call <see cref="Time.Update"/> once per frame to advance all time values.</summary>
+    public static class Time
+    {
+        #region Variables
+
+        #region Time
+
+        public static double timeAsDouble { get; private set; }
+        public static float time
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)timeAsDouble;
+        }
+
+        public static double unscaledTimeAsDouble { get; private set; }
+        public static float unscaledTime
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)unscaledTimeAsDouble;
+        }
+
+        public static double deltaTimeAsDouble { get; private set; }
+        public static float deltaTime
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)deltaTimeAsDouble;
+        }
+
+        public static double unscaledDeltaTimeAsDouble { get; private set; }
+        public static float unscaledDeltaTime
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)unscaledDeltaTimeAsDouble;
+        }
+
+        public static double fixedTimeAsDouble { get; private set; }
+        public static float fixedTime
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)fixedTimeAsDouble;
+        }
+
+        public static double fixedDeltaTimeAsDouble { get; private set; } = 1.0 / 50.0;
+        public static float fixedDeltaTime
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)fixedDeltaTimeAsDouble;
+        }
+
+        public static double smoothDeltaTimeAsDouble
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => smoothSampleCount > 0 ? smoothDeltaAccumulator / smoothSampleCount : 0.0;
+        }
+
+        #endregion
+
+        public static int frameCount { get; private set; }
+
+        public static int fixedFrameCount { get; private set; }
+
+        public static float timeScale = 1f;
+        public static float maxDeltaTime = 1f / 3f; // No less than ~3 FPS
+
+        #region Smooth Time
+
+        const int SmoothSamples = 5;
+
+        public static double smoothDeltaAccumulator {  get; private set; }
+        public static int smoothSampleCount { get; private set; }
+        public static readonly double[] smoothBuffer = new double[SmoothSamples];
+        public static int smoothBufferIndex { get; private set; }
+
+        #endregion
+
+        public static double fixedAccumulator { get; private set; }
+
+        public static float FixedRate
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)(1.0 / fixedDeltaTimeAsDouble);
+        }
+
+        public static float Fps
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => unscaledDeltaTimeAsDouble > 0.0 ? (float)(1.0 / unscaledDeltaTimeAsDouble) : 0f;
+        }
+
+        public static float SmoothFps
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => smoothDeltaTimeAsDouble > 0.0 ? (float)(1.0 / smoothDeltaTimeAsDouble) : 0f;
+        }
+
+        public static float FixedAlpha
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (float)(fixedAccumulator / fixedDeltaTimeAsDouble);
+        }
+
+        public static bool FixedUpdatedThisFrame
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => fixedAccumulator < fixedDeltaTimeAsDouble;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Advances all time values by the given raw frame duration. <br></br>
+        /// Call this exactly once per frame before any game logic.
+        /// </summary>
+        /// <param name="deltaTime">Raw elapsed time in seconds since the last frame.</param>
+        public static void Update(double deltaTime)
+        {
+            double rawDelta = Math.Min(deltaTime, maxDeltaTime);
+
+            unscaledDeltaTimeAsDouble = rawDelta;
+            unscaledTimeAsDouble += rawDelta;
+
+            double scaledDelta = rawDelta * timeScale;
+            deltaTimeAsDouble = scaledDelta;
+            timeAsDouble += scaledDelta;
+
+            smoothDeltaAccumulator -= smoothBuffer[smoothBufferIndex];
+            smoothBuffer[smoothBufferIndex] = rawDelta;
+            smoothDeltaAccumulator += rawDelta;
+            smoothBufferIndex = (smoothBufferIndex + 1) % SmoothSamples;
+            if (smoothSampleCount < SmoothSamples)
+                smoothSampleCount++;
+
+            fixedAccumulator += scaledDelta;
+            while(fixedAccumulator >= fixedDeltaTimeAsDouble)
+            {
+                fixedTimeAsDouble += fixedDeltaTimeAsDouble;
+                fixedAccumulator -= fixedDeltaTimeAsDouble;
+                fixedFrameCount++;
+            }
+
+            frameCount++;
+        }
+
+        /// <summary>
+        /// Advances all time values by the given raw frame duration. <br></br>
+        /// Call this exactly once per frame before any game logic.
+        /// </summary>
+        /// <param name="deltaTime">Raw elapsed time in seconds since the last frame.</param>
+        public static void Update(float deltaTime) => Update((double)deltaTime);
+
+        public static void SetFixedDeltaTime(double seconds)
+        {
+            if (seconds <= 0.0)
+                throw new ArgumentOutOfRangeException(nameof(seconds), "Fixed delta time must be positive.");
+
+            fixedDeltaTimeAsDouble = seconds;
+        }
+
+        public static void SetFixedRate(double hz)
+        {
+            if (hz <= 0.0)
+                throw new ArgumentOutOfRangeException(nameof(hz), "Fixed rate must be positive.");
+
+            fixedDeltaTimeAsDouble = 1.0 / hz;
+        }
+
+        /// <summary>Reset all time states to zero.</summary>
+        public static void Reset()
+        {
+            timeAsDouble = 0.0;
+            unscaledTimeAsDouble = 0.0;
+
+            deltaTimeAsDouble = 0.0;
+            unscaledDeltaTimeAsDouble = 0.0;
+
+            fixedTimeAsDouble = 0.0;
+            fixedAccumulator = 0.0;
+
+            frameCount = 0;
+            fixedFrameCount = 0;
+            timeScale = 1f;
+
+            smoothDeltaAccumulator = 0.0;
+            smoothSampleCount = 0;
+            smoothBufferIndex = 0;
+            Array.Clear(smoothBuffer, 0, SmoothSamples);
+        }
+    }
+}
