433ce37497ef5de8a177a186ab6ee478757d53c4
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-03-01 11:36:34 +0100

    I give up typing

--- Essence/Input/Input.cs
+++ Essence/Input/Input.cs
@@ -46,7 +46,84 @@ namespace Essence.Input
 
         static Input()
         {
+            // TODO Complete the list (I am to lazy atm)
+            sdlKeyMap = new Dictionary<SDL.KeyCode, KeyCode>(KeyCapacity)
+            {
+                { SDL.KeyCode.Space, KeyCode.Space },
+                { SDL.KeyCode.Space, KeyCode.Apostrophe },
+                { SDL.KeyCode.Space, KeyCode.Comma },
+                { SDL.KeyCode.Space, KeyCode.Minus },
+                { SDL.KeyCode.Space, KeyCode.Period },
+                { SDL.KeyCode.Space, KeyCode.Slash },
 
+                { SDL.KeyCode.Alpha0, KeyCode.Alpha0 },
+                { SDL.KeyCode.Alpha1, KeyCode.Alpha1 },
+                { SDL.KeyCode.Alpha2, KeyCode.Alpha2 },
+                { SDL.KeyCode.Alpha3, KeyCode.Alpha3 },
+                { SDL.KeyCode.Alpha4, KeyCode.Alpha4 },
+                { SDL.KeyCode.Alpha5, KeyCode.Alpha5 },
+                { SDL.KeyCode.Alpha6, KeyCode.Alpha6 },
+                { SDL.KeyCode.Alpha7, KeyCode.Alpha7 },
+                { SDL.KeyCode.Alpha8, KeyCode.Alpha8 },
+                { SDL.KeyCode.Alpha9, KeyCode.Alpha9 },
+
+                { SDL.KeyCode.SemiColon, KeyCode.Semicolon },
+                { SDL.KeyCode.Equals, KeyCode.Equal },
+
+                { SDL.KeyCode.A, KeyCode.A },
+                { SDL.KeyCode.B, KeyCode.B },
+                { SDL.KeyCode.C, KeyCode.C },
+                { SDL.KeyCode.D, KeyCode.D },
+                { SDL.KeyCode.E, KeyCode.E },
+                { SDL.KeyCode.F, KeyCode.F },
+                { SDL.KeyCode.G, KeyCode.G },
+                { SDL.KeyCode.H, KeyCode.H },
+                { SDL.KeyCode.I, KeyCode.I },
+                { SDL.KeyCode.J, KeyCode.J },
+                { SDL.KeyCode.K, KeyCode.K },
+                { SDL.KeyCode.L, KeyCode.L },
+                { SDL.KeyCode.M, KeyCode.M },
+                { SDL.KeyCode.N, KeyCode.N },
+                { SDL.KeyCode.O, KeyCode.O },
+                { SDL.KeyCode.P, KeyCode.P },
+                { SDL.KeyCode.Q, KeyCode.Q },
+                { SDL.KeyCode.R, KeyCode.R },
+                { SDL.KeyCode.S, KeyCode.S },
+                { SDL.KeyCode.T, KeyCode.T },
+                { SDL.KeyCode.U, KeyCode.U },
+                { SDL.KeyCode.V, KeyCode.V },
+                { SDL.KeyCode.W, KeyCode.W },
+                { SDL.KeyCode.X, KeyCode.X },
+                { SDL.KeyCode.Y, KeyCode.Y },
+                { SDL.KeyCode.Z, KeyCode.Z },
+
+                { SDL.KeyCode.LeftBracket, KeyCode.LeftBracket },
+                { SDL.KeyCode.Backslash, KeyCode.BackSlash },
+                { SDL.KeyCode.RightBracket, KeyCode.RightBracket },
+                // GraveAccent
+
+                { SDL.KeyCode.Escape, KeyCode.RightBracket },
+                { SDL.KeyCode.RightBracket, KeyCode.RightBracket },
+                { SDL.KeyCode.Tab, KeyCode.RightBracket },
+                { SDL.KeyCode.Backspace, KeyCode.Backspace },
+                { SDL.KeyCode.Insert, KeyCode.Insert },
+                { SDL.KeyCode.Delete, KeyCode.Delete },
+
+                { SDL.KeyCode.Up, KeyCode.Up },
+                { SDL.KeyCode.Down, KeyCode.Down },
+                { SDL.KeyCode.Left, KeyCode.Left },
+                { SDL.KeyCode.Right, KeyCode.Right },
+
+                { SDL.KeyCode.LeftShift, KeyCode.LeftShift },
+                { SDL.KeyCode.LeftCtrl, KeyCode.LeftControl },
+                { SDL.KeyCode.LeftAlt, KeyCode.LeftAlt },
+                // Left Super
+                { SDL.KeyCode.RightShift, KeyCode.RightShift },
+                { SDL.KeyCode.RightCtrl, KeyCode.RightControl },
+                { SDL.KeyCode.RightAlt, KeyCode.RightAlt },
+                // Right Super
+                { SDL.KeyCode.Z, KeyCode.Z },
+            };
         }
     }
 }
